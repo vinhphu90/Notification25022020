6c618abea12edd08b3123bdb812826a2fe38d1c6
vinhphu90 <61481544+vinhphu90@users.noreply.github.com>
2020-05-06 21:53:06 +0700

    channel id version 0

--- app/src/main/java/com/examplevinhphutvp/notification25022020/MainActivity.java
+++ app/src/main/java/com/examplevinhphutvp/notification25022020/MainActivity.java
@@ -3,7 +3,12 @@ package com.examplevinhphutvp.notification25022020;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.app.NotificationCompat;
 
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
+import android.content.Intent;
 import android.graphics.BitmapFactory;
+import android.os.Build;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -11,6 +16,7 @@ import android.widget.Button;
 public class MainActivity extends AppCompatActivity {
     Button mBtnTao,mBtnDong ;
     String CHANNEL_ID = "channel_01";
+    int  mNotificationID = 1;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -33,6 +39,14 @@ public class MainActivity extends AppCompatActivity {
     }
 
     private void CreateNotification() {
+        //Intent : thao tac voi Notification
+        Intent intent = new Intent(this,MainActivity.class);
+        PendingIntent pendingIntent =
+                PendingIntent.getActivity(
+                        this,
+                        0,
+                        intent,
+                        PendingIntent.FLAG_CANCEL_CURRENT);
         NotificationCompat.Builder build = new NotificationCompat.Builder(this,CHANNEL_ID)
                 .setSmallIcon(R.mipmap.ic_launcher)
                 .setContentTitle("Có thông báo mới")
@@ -43,5 +57,19 @@ public class MainActivity extends AppCompatActivity {
                                 BitmapFactory.decodeResource(
                                         null ,
                                         R.drawable.ic_launcher_background)));
+
+        // hien thi ra Notification
+        NotificationManager notificationManager =
+                (NotificationManager) getSystemService(NOTIFICATION_SERVICE);
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O){
+            CharSequence Name = "Notification";
+            NotificationChannel notificationChannel =
+                    new NotificationChannel(CHANNEL_ID,Name,NotificationManager.IMPORTANCE_LOW);
+            // chế độ rung
+            notificationChannel.enableVibration(true);
+            notificationManager.createNotificationChannel(notificationChannel);
+        }
+
+        notificationManager.notify(mNotificationID,build.build());
     }
 }
